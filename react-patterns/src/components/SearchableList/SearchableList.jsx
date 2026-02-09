f6b61ffa4a00f08771193efb3e343024a63c886b
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-02-08 23:15:31 -0500

    Adding debounce to search text

--- react-patterns/src/components/SearchableList/SearchableList.jsx
+++ react-patterns/src/components/SearchableList/SearchableList.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef();
   const [searchText, setSearchText] = useState('');
 
   const searchResults = items.filter((item) =>
     JSON.stringify(item).toLowerCase().includes(searchText.toLowerCase()),
   );
 
+  //adding debounce
   function handleChange(event) {
-    setSearchText(event.target.value);
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSearchText(event.target.value);
+    }, 500);
   }
 
   return (
