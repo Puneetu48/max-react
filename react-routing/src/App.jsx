b9501aa29b711c0f284f2d554f447c371bc25209
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-02-25 22:55:33 -0500

    Basic react routing concepts 2

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
+import ProductDetailsPage from './pages/ProductDetails';
 
 const router = createBrowserRouter([
   {
@@ -10,8 +11,12 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage /> },
+      { index: true, element: <HomePage /> }, //index: true = path: ''
+      {
+        path: 'products',
+        element: <ProductsPage />,
+      },
+      { path: 'products/:productId', element: <ProductDetailsPage /> },
     ],
   },
 ]);
--- react-routing/src/pages/ProductDetails.jsx
+++ react-routing/src/pages/ProductDetails.jsx
@@ -1,7 +1,17 @@
+import { useParams, Link } from 'react-router-dom';
+
 function ProductDetailsPage() {
+  const params = useParams();
+
   return (
     <>
       <h1>Product Details!</h1>
+      <p>{params.productId}</p>
+      <p>
+        <Link to='..' relative='path'>
+          Back
+        </Link>
+      </p>
     </>
   );
 }
