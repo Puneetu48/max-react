a71056497b0eadaaf86aa63eaad5ed3ae2e3a077
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-03-04 07:21:54 -0500

    Fixing errors

--- custom-hooks/src/components/AvailablePlaces.jsx
+++ custom-hooks/src/components/AvailablePlaces.jsx
@@ -21,7 +21,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
           const sortedPlaces = sortPlacesByDistance(
             places,
             position.coords.latitude,
-            position.coords.longitude
+            position.coords.longitude,
           );
           setAvailablePlaces(sortedPlaces);
           setIsFetching(false);
@@ -39,16 +39,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
   }, []);
 
   if (error) {
-    return <Error title="An error occurred!" message={error.message} />;
+    return <Error title='An error occurred!' message={error.message} />;
   }
 
   return (
     <Places
-      title="Available Places"
+      title='Available Places'
       places={availablePlaces}
       isLoading={isFetching}
-      loadingText="Fetching place data..."
-      fallbackText="No places available."
+      loadingText='Fetching place data...'
+      fallbackText='No places available.'
       onSelectPlace={onSelectPlace}
     />
   );
